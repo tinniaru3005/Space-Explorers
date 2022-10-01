77f31a1a7914fdeb7b1f8fdf48e3888cdf477671
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-10-01 20:36:01 +0530

    Modular

--- js/callbacks.js
+++ js/callbacks.js
@@ -16,6 +16,12 @@ function getJSON(url, callback) {
   xhr.send();
 }
 
+function getProfiles(json) {
+  json.people.map(person => {
+    getJSON(wikiUrl + person.name, generateHTML);
+  });
+}
+
 // Generate the markup for each profile
 function generateHTML(data) {
   const section = document.createElement('section');
@@ -39,10 +45,6 @@ function generateHTML(data) {
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, (json) => {
-    json.people.map(person => {
-      getJSON(wikiUrl + person.name, generateHTML);
-    });
-  });
+  getJSON(astrosUrl, getProfiles);
   event.target.remove();
 });
