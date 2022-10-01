923c9fb65f6311a4eda2d6479ff4812539e0dcf2
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-10-01 23:00:54 +0530

    Add Promises

--- js/promises.js
+++ js/promises.js
@@ -3,47 +3,63 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+function getJSON(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        return callback(data);
+      } else {
+        reject( Error(xhr.statusText) );
+      }
+    };
+    xhr.onerror = () => reject( Error('A network error occurred.') );
+    xhr.send();
+  });
 }
 
 function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
+  const profiles = json.people.map( person => {
+    return getJSON(wikiUrl + person.name);      
+  });
+  return Promise.all(profiles);
 }
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  // Check if request returns a 'standard' page from Wiki
-  if (data.type === 'standard') {
-    section.innerHTML = `
-      <img src=${data.thumbnail.source}>
-      <h2>${data.title}</h2>
-      <p>${data.description}</p>
-      <p>${data.extract}</p>
-    `;
-  } else {
-    section.innerHTML = `
-      <img src="img/profile.jpg" alt="ocean clouds seen from space">
-      <h2>${data.title}</h2>
-      <p>Results unavailable for ${data.title}</p>
-      ${data.extract_html}
-    `;
-  }
+  data.map( person => {
+    const section = document.createElement('section');
+    peopleList.appendChild(section);
+    // Check if request returns a 'standard' page from Wiki
+    if (person.type === 'standard') {
+      section.innerHTML = `
+        <img src=${person.thumbnail.source}>
+        <h2>${person.title}</h2>
+        <p>${person.description}</p>
+        <p>${person.extract}</p>
+      `;
+    } else {
+      section.innerHTML = `
+        <img src="img/profile.jpg" alt="ocean clouds seen from space">
+        <h2>${person.title}</h2>
+        <p>Results unavailable for ${person.title}</p>
+        ${person.extract_html}
+      `;
+    }
+  });
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
-  event.target.remove();
+  event.target.textContent = "Loading...";
+
+  getJSON(astrosUrl)
+    .then(getProfiles)
+    .then(generateHTML)
+    .catch( err => {
+      peopleList.innerHTML = '<h3>Something went wrong</h3>';
+      console.log(err);
+    })
+    .finally( () => event.target.remove());
 });
